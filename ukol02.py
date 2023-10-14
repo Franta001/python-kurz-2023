103319db917f30a89b306abc19a4bf268011ee65
gagugo <82519190+gagugo@users.noreply.github.com>
2023-10-14 14:44:15 +0200

    Update ukol02.py

--- ukol02.py
+++ ukol02.py
@@ -21,3 +21,30 @@ if pocet > sklad[produkt]:
     if pocet > sklad[produkt]:
         sklad = sklad.pop(produkt)
     print(sklad)
+
+'''
+Komentar (M. Jurosz)
+zacatek dobry, ale program spatne resi pokud soucastka neni na sklade, protoze vleze tak ci tak do otazky pocet, coz je pro uzivatele nesmysl.
+Tedy mel by jsi cyklus zanorit do sebe, aby v pripade ze neexistuje mnozstvi program skoncil.
+produkt = input("Zadej kod soucastky: ")
+if produkt in sklad: 
+    print(f"{produkt} je na skladě.")  
+    pocet = int(input("Zadej počet kusů: "))
+    if pocet <= sklad[produkt]:
+        print(f"{produkt} je v požadovaném množství na skladě a zbyde {sklad[produkt]-pocet} ks.")
+    if pocet > sklad[produkt]:
+        print(f"{produkt} je dostupný v omezeném množství {sklad[produkt]-pocet} ks.")
+        if pocet > sklad[produkt]:
+            sklad = sklad.pop(produkt)
+        print(sklad)
+else: 
+    print(f"Bohužel, součástka {produkt} není na skladě.")
+
+Dale je jeste nesikovne napsana veta na radku 20
+Protoze ve chvili kdy vezme vice mnozstvi nez je na sklade tak program vrati vetu:
+"je dostupný v omezeném množství -1 ks." tedy spis bych cekal informaci ze dostane o tolik mene kusu
+nebo napriklad ze dostane jen 54 kusu misto pozadovanych 55 atd.
+
+Beru jako drobne chyby, jen v budoucich ukolech na to davat pozor.
+Diky
+'''
